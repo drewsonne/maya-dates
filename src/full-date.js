7a50ba8c16584b6eff5708f49bde2f6be97ab90d
Drew J. Sonne <drew.sonne@gmail.com>
2019-12-29 21:03:31 +0000

    Updated documentation

--- src/full-date.js
+++ src/full-date.js
@@ -1,14 +1,27 @@
+/**
+ * An encapsulation of a LongCount and Calendar Round which match each other.
+ */
 class FullDate {
   /**
-   *
    * @param {CalendarRound} cr
    * @param {LongCount} lc
    */
   constructor (cr, lc) {
+    /**
+     * @type {CalendarRound}
+     */
     this.cr = cr
+
+    /**
+     * @type {LongCount}
+     */
     this.lc = lc
   }
 
+  /**
+   * Render the FullDate as a string of both the CR and the LC
+   * @returns {string}
+   */
   toString () {
     return `${this.cr} ${this.lc}`
   }
--- src/operations/calendar-round-wildcard.js
+++ src/operations/calendar-round-wildcard.js
@@ -1,30 +1,56 @@
+/** @ignore */
 const CalendarRound = require('../cr/calendar-round')
+/** @ignore */
 const origin = require('../cr/index').origin
 
-class CalendarRoundIterator {
+/**
+ * Used to iterate through the entire cycle of 18,980 days for the full
+ * permutation of Haab and 260-day count.
+ * @ignore
+ */
+class _CalendarRoundIterator {
   /**
    *
    * @param {CalendarRound} date
    */
   constructor (date) {
+    /**
+     * @type {CalendarRound}
+     */
+    this.current = undefined
+
+    /**
+     * @type boolean
+     */
+    this.is_first = undefined
+
     if (date === undefined) {
       date = origin
     }
+    /**
+     * @type {CalendarRound}
+     */
     this.date = date
     this.reset()
   }
 
+  /**
+   * Reset this iterator so that it can be reused.
+   */
   reset () {
     this.current = this.date
     this.is_first = true
   }
 
+  /**
+   * Move to the next position in the iterator or end the iteration.
+   * @return {{value: null, done: boolean}|{value: CalendarRound, done: boolean}}
+   */
   next () {
     if (this.is_first) {
       this.is_first = false
       return {value: this.current, done: false}
     }
-    this.count += 1
     let next = this.current.next()
     if (next.equal(this.date)) {
       return {value: null, done: true}
@@ -36,16 +62,32 @@ class CalendarRoundIterator {
 
 }
 
-const iter = new CalendarRoundIterator()
+/**
+ * A reusable singleton instance of the CalendarRoundIterator
+ * @ignore
+ * @type {_CalendarRoundIterator}
+ */
+const iter = new _CalendarRoundIterator()
 
+/**
+ * Given a Calendar Round with a wildcard, calculate all possible matching
+ * fully qualified Calendar Rounds.
+ */
 class CalendarRoundWildcard {
   /**
    * @param {CalendarRound} cr
    */
   constructor (cr) {
+    /**
+     * @type {CalendarRound}
+     */
     this.cr = cr
   }
 
+  /**
+   * Run calculation to find all fully qualified Calendar Rounds
+   * @return {CalendarRound[]}
+   */
   run () {
     let potentials = []
     // Iterate through dates and compare
