d05ba87b951c34060f69fd17501b9ff6c8438a5b
Drew <drew.sonne@blis.com>
2020-03-12 07:46:53 +0000

    Disable tests

--- test/full-date.test.js
+++ test/full-date.test.js
@@ -5,3 +5,18 @@ test('full date rendering', () => {
     .parse('1Ok * * 9.*.10.10.10');
   expect(`${fullDate}`).toBe('1 Ok * *  9. *.10.10.10');
 });
+
+// const stream = [
+//   new Text('13Imix'),
+//   new Text('4Muwan'),
+//   new Numeric('1.1.2.1')
+// ];
+
+
+test('full date equlaity', () => {
+  const fullDates = [
+    [
+      '13 Imix 4 Muwan 1.1.2.1',
+    ]
+  ]
+})
