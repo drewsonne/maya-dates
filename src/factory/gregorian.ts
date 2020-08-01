148a6a9c794c457c30e2478b4627f89e9ab81256
Drew <drew.sonne@blis.com>
2020-08-01 12:39:55 +0100

    Add gregorian factory

--- src/factory/gregorian.ts
+++ src/factory/gregorian.ts
@@ -3,6 +3,7 @@ import GregorianCalendarDate from "../lc/western/gregorian";
 export default class GregorianFactory {
 
   parse(gregorian: string): GregorianCalendarDate {
-
+    let result: GregorianCalendarDate = new GregorianCalendarDate(10);
+    return result
   }
 }
